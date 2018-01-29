6ea4cc3898b81946ca53b7c73d07fbc5c80aeadc
amaro@broadinstitute.org <amaro@broadinstitute.org>
2018-01-29 17:35:40 -0500

    vectorizing inner loops

--- deTiN_SSNV_based_estimate.py
+++ deTiN_SSNV_based_estimate.py
@@ -12,7 +12,7 @@ class model:
      TiN estimate : model.TiN
      Somatic classification of SSNVs : model.E_z (E_z > 0.5 -> somatic)"""
 
-    def __init__(self, candidate_sites, p_somatic,resolution = 101, f_thresh = 0.15 , depth = 15):
+    def __init__(self, candidate_sites, p_somatic, resolution=101, f_thresh=0.15, depth=15):
         # variables follow notation:
         # ac = allele count n = normal t = tumor
 
@@ -33,7 +33,8 @@ class model:
         self.t_depth = self.t_alt_count + self.t_ref_count
         self.tumor_f = np.true_divide(self.t_alt_count, self.t_depth)
         self.number_of_sites = len(self.n_alt_count)
-        self.candidate_sites = np.logical_and(np.logical_and(self.tumor_f > f_thresh, self.t_depth > depth), self.n_depth > depth)
+        self.candidate_sites = np.logical_and(np.logical_and(self.tumor_f > f_thresh, self.t_depth > depth),
+                                              self.n_depth > depth)
         # hyperparameter
         self.p_somatic = p_somatic
 
@@ -70,51 +71,50 @@ class model:
 
     def generate_conditional_ps(self):
         # p(TiN|Somatic) and p(TiN|Germline)
-        n_af_w = np.zeros([self.number_of_sites, len(self.af)])
-        t_af_w = np.zeros([self.number_of_sites, len(self.af)])
         t_het_direction = np.ones([self.number_of_sites, len(self.af)])
-        t_het_direction[:, 0:np.int(np.round(np.true_divide(len(self.af),2)))] = -1
-        for i, f in enumerate(self.af):
-            n_af_w[:, i] = beta._cdf(f,self.n_alt_count+1,self.n_ref_count+1) - beta._cdf(f - 0.005,self.n_alt_count+1,self.n_ref_count+1)
-            t_af_w[:, i] = beta._cdf(f,self.t_alt_count+1,self.t_ref_count+1) - beta._cdf(f - 0.005,self.t_alt_count+1,self.t_ref_count+1)
-            # ac given somatic
-            t_af = np.multiply(f, self.n_depth)
-            n_ac_given_tin = np.multiply(t_af[:, np.newaxis], self.CN_ratio)
+        t_het_direction[:, 0:np.int(np.round(np.true_divide(len(self.af), 2)))] = -1
+        self.afexp = np.repeat(np.expand_dims(self.af, 1), self.number_of_sites, axis=1).T
+        t_af_w = beta._cdf(self.afexp, np.expand_dims(self.t_alt_count + 1, 1),
+                           np.expand_dims(self.t_ref_count + 1, 1)) - beta._cdf(self.afexp - 0.005,
+                                                                                np.expand_dims(self.t_alt_count + 1, 1),
+                                                                                np.expand_dims(self.t_ref_count + 1, 1))
+        f_t_af = 0.5 - np.abs(0.5 - self.afexp)
+        t_af = np.multiply(self.afexp, np.expand_dims(self.n_depth, 1))
 
-            # ac given heterozygous
-            f_t_af = 0.5 - np.abs((.5 - f))
-            psi_t_af = 0.5 - f_t_af
-            psi_t_af = np.multiply(psi_t_af, t_het_direction[:, i])
-            exp_f = 0.5 + np.multiply(psi_t_af[:, np.newaxis], self.CN_ratio)
+        psi_t_af = 0.5 - f_t_af
+        psi_t_af = np.multiply(psi_t_af, t_het_direction)
+        for TiN_idx, TiN in enumerate(self.TiN_range):
+            n_ac_given_tin = np.multiply(t_af, np.expand_dims(self.CN_ratio[:, TiN_idx], 1))
+            exp_f = 0.5 + np.multiply(psi_t_af, np.expand_dims(self.CN_ratio[:, TiN_idx], 1))
             n_het_ac_given_tin = np.multiply(exp_f, self.n_depth[:, np.newaxis])
 
-            for TiN_idx, TiN in enumerate(self.TiN_range):
-                self.p_TiN_given_S[:, TiN_idx] += np.multiply(beta._cdf(self.normal_f[:]+.01, n_ac_given_tin[:, TiN_idx] + 1,
-                                                                       self.n_depth[:] - n_ac_given_tin[:,
-                                                                                         TiN_idx] + 1) - beta._cdf(self.normal_f[:], n_ac_given_tin[:, TiN_idx] + 1,
-                                                                       self.n_depth[:] - n_ac_given_tin[:,
-                                                                                         TiN_idx] + 1),
-                                                              t_af_w[:, i])
-                self.p_TiN_given_het[:, TiN_idx] += np.multiply(
-                    beta._cdf(self.normal_f[:]+.01, n_het_ac_given_tin[:, TiN_idx] + 1,
-                             self.n_depth[:] - n_het_ac_given_tin[:, TiN_idx] + 1) -
-                    beta._cdf(self.normal_f[:], n_het_ac_given_tin[:, TiN_idx] + 1,
-                             self.n_depth[:] - n_het_ac_given_tin[:, TiN_idx] + 1) ,
-                    t_af_w[:, i])
-
-        self.p_artifact = beta._cdf(self.normal_f+.01,self.t_alt_count+1,self.t_ref_count+1) - beta._cdf(self.normal_f,self.t_alt_count+1,self.t_ref_count+1)
+            self.p_TiN_given_S[:, TiN_idx] += np.sum(
+                np.multiply(beta._cdf(np.expand_dims(self.normal_f[:] + .01, 1), n_ac_given_tin + 1,
+                                      self.n_depth[:, np.newaxis] - n_ac_given_tin + 1) -
+                            beta._cdf(np.expand_dims(self.normal_f[:], 1), n_ac_given_tin + 1,
+                                      self.n_depth[:, np.newaxis] - n_ac_given_tin + 1), t_af_w), axis=1)
+            self.p_TiN_given_het[:, TiN_idx] += np.sum(
+                np.multiply(beta._cdf(np.expand_dims(self.normal_f[:] + .01, 1), n_het_ac_given_tin + 1,
+                                      self.n_depth[:, np.newaxis] - n_het_ac_given_tin + 1) -
+                            beta._cdf(np.expand_dims(self.normal_f[:], 1), n_het_ac_given_tin + 1,
+                                      self.n_depth[:, np.newaxis] - n_het_ac_given_tin + 1), t_af_w), axis=1)
+        self.p_artifact = beta._cdf(self.normal_f + .01, self.t_alt_count + 1, self.t_ref_count + 1) - beta._cdf(
+            self.normal_f, self.t_alt_count + 1, self.t_ref_count + 1)
         self.p_TiN_given_G = np.multiply(1 - self.p_artifact[:, np.newaxis], self.p_TiN_given_het) + np.multiply(
             self.p_artifact[:, np.newaxis], 1 - self.p_TiN_given_het)
+
     def expectation_of_z_given_TiN(self):
         # E step
-        numerator = self.p_somatic * (self.p_TiN_given_S[:,self.TiN])
+        numerator = self.p_somatic * (self.p_TiN_given_S[:, self.TiN])
         denominator = numerator + np.array([1 - self.p_somatic] * np.nan_to_num(self.p_TiN_given_G[:, self.TiN]))
         self.E_z = np.nan_to_num(np.true_divide(numerator, denominator))
 
     def maximize_TiN_likelihood(self):
         # M step
-        self.TiN_likelihood = np.nansum(np.multiply(self.E_z[self.candidate_sites, np.newaxis], np.ma.log(self.p_TiN_given_S[self.candidate_sites,:])), axis=0) + \
-                                np.nansum(np.multiply(1 - self.E_z[self.candidate_sites, np.newaxis], np.ma.log(self.p_TiN_given_G[self.candidate_sites,:])),axis=0)
+        self.TiN_likelihood = np.nansum(np.multiply(self.E_z[self.candidate_sites, np.newaxis],
+                                                    np.ma.log(self.p_TiN_given_S[self.candidate_sites, :])), axis=0) + \
+                              np.nansum(np.multiply(1 - self.E_z[self.candidate_sites, np.newaxis],
+                                                    np.ma.log(self.p_TiN_given_G[self.candidate_sites, :])), axis=0)
         self.TiN = np.argmax(self.TiN_likelihood)
 
     def perform_inference(self):
@@ -133,10 +133,12 @@ class model:
         print 'SSNV based TiN estimate converged: TiN = ' + str(self.TiN_range[self.TiN])
         self.TiN = self.TiN_range[self.TiN]
 
-        posterior = np.exp(self.TiN_likelihood- np.nanmax(self.TiN_likelihood))
+        posterior = np.exp(self.TiN_likelihood - np.nanmax(self.TiN_likelihood))
         self.CI_tin_low = self.TiN_range[
-            next(x[0] for x in enumerate(np.cumsum(np.ma.masked_array((np.true_divide(posterior, np.nansum(posterior))))))
-             if x[1] > 0.025)]
+            next(x[0] for x in
+                 enumerate(np.cumsum(np.ma.masked_array((np.true_divide(posterior, np.nansum(posterior))))))
+                 if x[1] > 0.025)]
         self.CI_tin_high = self.TiN_range[
-            next(x[0] for x in enumerate(np.cumsum(np.ma.masked_array((np.true_divide(posterior, np.nansum(posterior))))))
-            if x[1] > 0.975)]
+            next(x[0] for x in
+                 enumerate(np.cumsum(np.ma.masked_array((np.true_divide(posterior, np.nansum(posterior))))))
+                 if x[1] > 0.975)]
