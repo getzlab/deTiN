ba0187b79d100d1bab04729bf846e6b4a6572b69
amaro@broadinstitute.org <amaro@broadinstitute.org>
2018-02-20 08:33:36 -0500

    small change to check number of segments and hets before performing inference using aSCNAs

--- deTiN.py
+++ deTiN.py
@@ -13,7 +13,8 @@ import deTiN_aSCNA_based_estimate as dascna
 class input:
     """class which holds the required detin somatic data prior to model"""
 
-    def __init__(self, args, ascna_probe_number_filter = 200,ascna_SNP_number_filter = 20,coverage_threshold = 15, SSNV_af_threshold = 0.15,aSCNA_variance_threshold=0.025 ):
+    def __init__(self, args, ascna_probe_number_filter=200, ascna_SNP_number_filter=20, coverage_threshold=15,
+                 SSNV_af_threshold=0.15, aSCNA_variance_threshold=0.025):
 
         # related to inputs from command line
         self.call_stats_file = args.mutation_data_path
@@ -80,7 +81,6 @@ class input:
         except AttributeError:
             self.aSCNA_variance_threshold = 'NA'
 
-
         # related to inputs from class functions
         self.call_stats_table = []
         self.seg_table = []
@@ -111,15 +111,17 @@ class input:
         self.call_stats_table['genomic_coord_x'] = du.hg19_to_linear_positions(
             np.array(self.call_stats_table['Chromosome']), np.array(self.call_stats_table['position']))
         self.n_calls_in = len(self.call_stats_table)
-        self.call_stats_table.reset_index(inplace=True,drop=True)
+        self.call_stats_table.reset_index(inplace=True, drop=True)
 
     def read_het_file(self):
         t_het_header = du.read_file_header(self.tumor_het_file)
         n_het_header = du.read_file_header(self.normal_het_file)
-        cols_t_type = {t_het_header[0]:str}
+        cols_t_type = {t_het_header[0]: str}
         cols_n_type = {n_het_header[0]: str}
-        tumor_het_table = pd.read_csv(self.tumor_het_file, '\t', index_col=False, low_memory=False, comment='#',dtype=cols_t_type)
-        normal_het_table = pd.read_csv(self.normal_het_file, '\t', index_col=False, low_memory=False, comment='#',dtype=cols_n_type)
+        tumor_het_table = pd.read_csv(self.tumor_het_file, '\t', index_col=False, low_memory=False, comment='#',
+                                      dtype=cols_t_type)
+        normal_het_table = pd.read_csv(self.normal_het_file, '\t', index_col=False, low_memory=False, comment='#',
+                                       dtype=cols_n_type)
         tumor_het_table = du.fix_het_file_header(tumor_het_table)
         normal_het_table = du.fix_het_file_header(normal_het_table)
         tumor_het_table['Chromosome'] = du.chr2num(np.array(tumor_het_table['CONTIG']))
@@ -138,8 +140,9 @@ class input:
 
     def read_seg_file(self):
         seg_header = du.read_file_header(self.seg_file)
-        cols_seg_type = {seg_header[0]:str}
-        self.seg_table = pd.read_csv(self.seg_file, '\t', index_col=False, low_memory=False, comment='#',dtype=cols_seg_type)
+        cols_seg_type = {seg_header[0]: str}
+        self.seg_table = pd.read_csv(self.seg_file, '\t', index_col=False, low_memory=False, comment='#',
+                                     dtype=cols_seg_type)
         self.seg_table = du.fix_seg_file_header(self.seg_table)
 
         self.seg_table['Chromosome'] = du.chr2num(np.array(self.seg_table['Chromosome']))
@@ -185,14 +188,15 @@ class input:
         self.annotate_call_stats_with_allelic_cn_data()
         if not self.indel_file == 'None':
             if not self.indel_type == 'None':
-                self.indel_table = du.read_indel_vcf(self.indel_file,self.seg_table,self.indel_type)
+                self.indel_table = du.read_indel_vcf(self.indel_file, self.seg_table, self.indel_type)
             else:
                 print 'Warning: if indels are provided you must also specify indel data source using --indel_data_type'
                 print 'no indels will be returned'
                 self.indel_file = 'None'
                 self.indel_type = 'None'
         self.read_het_file()
-        self.seg_table = du.filter_segments_based_on_size_f_and_tau(self.seg_table,self.aSCNA_thresh,self.ascna_probe_number_filter)
+        self.seg_table = du.filter_segments_based_on_size_f_and_tau(self.seg_table, self.aSCNA_thresh,
+                                                                    self.ascna_probe_number_filter)
         self.annotate_het_table()
         self.het_table = du.remove_sites_near_centromere_and_telomeres(self.het_table)
 
@@ -290,7 +294,8 @@ class output:
             sys.exit()
         pH1 = self.joint_posterior[self.TiN_int]
         pH0 = self.joint_posterior[0]
-        if np.true_divide(self.input.TiN_prior*pH1,(self.input.TiN_prior*pH1)+((1-self.input.TiN_prior)*pH0)) < 0.5 :
+        if np.true_divide(self.input.TiN_prior * pH1,
+                          (self.input.TiN_prior * pH1) + ((1 - self.input.TiN_prior) * pH0)) < 0.5:
             print 'insufficient evidence to justify TiN > 0'
             self.joint_posterior = np.zeros([self.input.resolution, 1])
             self.joint_posterior[0] = 1
@@ -302,25 +307,27 @@ class output:
         if self.input.weighted_classification == True:
             numerator = np.zeros(len(self.ssnv_based_model.p_TiN_given_S))
             denominator = np.zeros(len(self.ssnv_based_model.p_TiN_given_S))
-            for idx,p in enumerate(self.joint_posterior):
+            for idx, p in enumerate(self.joint_posterior):
                 if p > 0.001:
-                    num_iter = (p *self.ssnv_based_model.p_somatic * self.ssnv_based_model.p_TiN_given_S[:, idx])
+                    num_iter = (p * self.ssnv_based_model.p_somatic * self.ssnv_based_model.p_TiN_given_S[:, idx])
                     numerator = numerator + num_iter
-                    denom_iter = num_iter + p*(np.array(
+                    denom_iter = num_iter + p * (np.array(
                         [1 - self.ssnv_based_model.p_somatic] * np.nan_to_num(
                             self.ssnv_based_model.p_TiN_given_G[:, idx])))
                     denominator = denominator + denom_iter
 
         else:
-            numerator = self.ssnv_based_model.p_somatic * np.expand_dims(self.ssnv_based_model.p_TiN_given_S[:, self.TiN_int],1)
+            numerator = self.ssnv_based_model.p_somatic * np.expand_dims(
+                self.ssnv_based_model.p_TiN_given_S[:, self.TiN_int], 1)
             denominator = numerator + np.array(
-                [1 - self.ssnv_based_model.p_somatic] * np.expand_dims(np.nan_to_num(self.ssnv_based_model.p_TiN_given_G[:, self.TiN_int]),1))
+                [1 - self.ssnv_based_model.p_somatic] * np.expand_dims(
+                    np.nan_to_num(self.ssnv_based_model.p_TiN_given_G[:, self.TiN_int]), 1))
         self.SSNVs.loc[:, ('p_somatic_given_TiN')] = np.nan_to_num(np.true_divide(numerator, denominator))
         # expected normal allele fraction given TiN and tau
         af_n_given_TiN = np.multiply(self.ssnv_based_model.tumor_f, self.ssnv_based_model.CN_ratio[:, self.TiN_int])
         # probability of normal allele fraction less than or equal to predicted fraction
-        self.SSNVs.loc[:, 'p_outlier'] = self.ssnv_based_model.rv_normal_af.cdf(af_n_given_TiN +0.01)
-        if self.TiN_int == 0 :
+        self.SSNVs.loc[:, 'p_outlier'] = self.ssnv_based_model.rv_normal_af.cdf(af_n_given_TiN + 0.01)
+        if self.TiN_int == 0:
             print 'Estimated 0 TiN no SSNVs will be recovered outputing deTiN statistics for each site'
         elif self.use_outlier_threshold:
             # remove outliers mutations p(af_n >= E[af_n|TiN]) < 0.05
@@ -330,17 +337,17 @@ class output:
             self.SSNVs['judgement'][self.SSNVs['p_somatic_given_TiN'] > self.threshold] = 'KEEP'
         if not self.input.indel_file == 'None':
             print 'reclassifying indels'
-            indel_model = dssnv.model(self.input.indel_table, self.input.mutation_prior,self.input.resolution)
+            indel_model = dssnv.model(self.input.indel_table, self.input.mutation_prior, self.input.resolution)
             indel_model.generate_conditional_ps()
             self.indels = self.input.indel_table
-            numerator = indel_model.p_somatic * np.expand_dims(indel_model.p_TiN_given_S[:, self.TiN_int],1)
+            numerator = indel_model.p_somatic * np.expand_dims(indel_model.p_TiN_given_S[:, self.TiN_int], 1)
             denominator = numerator + np.array(
                 [1 - indel_model.p_somatic] * np.expand_dims(np.nan_to_num(
-                    indel_model.p_TiN_given_G[:, self.TiN_int]),1))
+                    indel_model.p_TiN_given_G[:, self.TiN_int]), 1))
             af_n_given_TiN = np.multiply(indel_model.tumor_f, indel_model.CN_ratio[:, self.TiN_int])
             self.indels.loc[:, ('p_somatic_given_TiN')] = np.nan_to_num(np.true_divide(numerator, denominator))
             self.indels.loc[:, 'p_outlier'] = indel_model.rv_normal_af.cdf(af_n_given_TiN)
-            if self.TiN_int == 0 :
+            if self.TiN_int == 0:
                 print 'Estimated 0 TiN no indels will be recovered outputing deTiN statistics for each site'
             elif self.use_outlier_threshold:
                 # remove outliers mutations p(af_n >= E[af_n|TiN]) < 0.05
@@ -348,6 +355,7 @@ class output:
                                                      self.indels['p_outlier'] >= 0.01)] = 'PASS'
             else:
                 self.indels['filter'][self.indels['p_somatic_given_TiN'] > self.threshold] = 'PASS'
+
 
 __version__ = '1.0'
 
@@ -373,10 +381,11 @@ def main():
                         help='Path to heterozygous site allele count data in normal. Generated by GATK4 GetBayesianHetCoverage'
                              'Required columns: CONTIG,POS,REF_COUNT and ALT_COUNT', required=True)
     parser.add_argument('--exac_data_path',
-                        help='Path to exac af > 0.01 pickle. Can be generated by downloading ExAC VCF and running build_exac_pickle', required=False)
+                        help='Path to exac af > 0.01 pickle. Can be generated by downloading ExAC VCF and running build_exac_pickle',
+                        required=False)
     parser.add_argument('--indel_data_path',
                         help='Path to candidate indels data.'
-                             'Supported formats: Strelka / MuTect2 VCFs', required=False,default='None')
+                             'Supported formats: Strelka / MuTect2 VCFs', required=False, default='None')
     parser.add_argument('--indel_data_type',
                         help='MuTect2 or Strelka'
                              'Caller used to generate indels', required=False, default='None')
@@ -389,57 +398,63 @@ def main():
                         , required=False, default=0.15)
     parser.add_argument('--aSCNA_threshold', help='minor allele fraction threshold for calling aSCNAs.'
                         , required=False, default=0.1)
-    parser.add_argument('--TiN_prior', help='expected frequency of TiN contamination in sequencing setting used for model selection',
+    parser.add_argument('--TiN_prior',
+                        help='expected frequency of TiN contamination in sequencing setting used for model selection',
                         required=False, default=0.5)
     parser.add_argument('--use_outlier_removal',
                         help='remove sites from recovered SSNVs where allele fractions significantly exceed predicted fraction',
                         required=False, default=True)
-    parser.add_argument('--resolution', help='number of TiN bins to consider default = 101 corresponds to 0.01 TiN levels'
+    parser.add_argument('--resolution',
+                        help='number of TiN bins to consider default = 101 corresponds to 0.01 TiN levels'
                         , required=False, default=101)
     parser.add_argument('--weighted_classification',
                         help='integrate variant classification over all values of TiN'
                         , required=False, default=False)
-    parser.add_argument('--ascna_probe_number_filter',help='number of probes to require for an aSCNA to be considered'
-                                                           ,required = False,default = 200)
+    parser.add_argument('--ascna_probe_number_filter', help='number of probes to require for an aSCNA to be considered'
+                        , required=False, default=200)
     parser.add_argument('--ascna_SNP_number_filter', help='number of probes to require for an aSCNA to be considered'
                         , required=False, default=20)
-    parser.add_argument('--coverage_threshold',help='number of reads required to use a site for TiN estimation',required=False,
+    parser.add_argument('--coverage_threshold', help='number of reads required to use a site for TiN estimation',
+                        required=False,
                         default=15)
     parser.add_argument('--SSNV_af_threshold', help='fraction of alternate alleles required for site to be used '
                                                     'for SSNV TiN estimation',
                         required=False,
                         default=.15)
-    parser.add_argument('--aSCNA_variance_threshold',help='variance of segment allele shift tolerated before removing segment '
-                                                          'as artifact',required=False,default=0.025)
-    parser.add_argument('--cancer_hot_spots', help='Optional BED file of cancer hot spot mutations which the user has a stronger prior on being somatic e.g. BRAF v600E mutations.'
-                                                   'The format of this file is Chromosome\tPosition\tProbability. Note this will override the mutation prior at these locations'
-                        , required=False, default= 'NA')
+    parser.add_argument('--aSCNA_variance_threshold',
+                        help='variance of segment allele shift tolerated before removing segment '
+                             'as artifact', required=False, default=0.025)
+    parser.add_argument('--cancer_hot_spots',
+                        help='Optional BED file of cancer hot spot mutations which the user has a stronger prior on being somatic e.g. BRAF v600E mutations.'
+                             'The format of this file is Chromosome\tPosition\tProbability. Note this will override the mutation prior at these locations'
+                        , required=False, default='NA')
     args = parser.parse_args()
     di = input(args)
     di.read_and_preprocess_data()
 
     # identify candidate mutations based on MuTect flags.
     # kept sites are flagged as KEEP or rejected for normal lod and/or alt_allele_in_normal
-    di.candidates = du.select_candidate_mutations(di.call_stats_table,di.exac_db_file)
+    di.candidates = du.select_candidate_mutations(di.call_stats_table, di.exac_db_file)
 
     # generate SSNV based model using candidate sites
-    ssnv_based_model = dssnv.model(di.candidates, di.mutation_prior, di.resolution,di.SSNV_af_threshold,di.coverage_threshold,di.CancerHotSpotsBED)
+    ssnv_based_model = dssnv.model(di.candidates, di.mutation_prior, di.resolution, di.SSNV_af_threshold,
+                                   di.coverage_threshold, di.CancerHotSpotsBED)
     ssnv_based_model.perform_inference()
-
+    ascna = False
     # identify aSCNAs and filter hets
-    if len(di.seg_table)>0:
+    if len(di.seg_table) > 0:
         di.aSCNA_hets = du.ensure_balanced_hets(di.seg_table, di.het_table)
-        if len(di.aSCNA_hets) == 0:
-            ascna_based_model = dascna.model(di.seg_table, di.het_table, di.resolution)
-            ascna_based_model.TiN = np.nan
-        else:
-            di.aSCNA_segs = du.identify_aSCNAs(di.seg_table, di.aSCNA_hets,di.aSCNA_thresh,di.ascna_SNP_number_filter,di.aSCNA_variance_threshold)
-            # generate aSCNA based model
-            ascna_based_model = dascna.model(di.aSCNA_segs, di.aSCNA_hets,di.resolution)
-            ascna_based_model.perform_inference()
-    else:
-        ascna_based_model = dascna.model(di.seg_table, di.het_table,di.resolution)
+        if len(di.aSCNA_hets) > 0:
+            di.aSCNA_segs = du.identify_aSCNAs(di.seg_table, di.aSCNA_hets, di.aSCNA_thresh, di.ascna_SNP_number_filter,
+                                               di.aSCNA_variance_threshold)
+            if len(di.aSCNA_segs) > 0:
+                ascna_based_model = dascna.model(di.aSCNA_segs, di.aSCNA_hets, di.resolution)
+                ascna_based_model.perform_inference()
+                ascna = True
+    if not ascna:
+        ascna_based_model = dascna.model(di.seg_table, di.het_table, di.resolution)
         ascna_based_model.TiN = np.nan
+
     # combine models and reclassify mutations
     do = output(di, ssnv_based_model, ascna_based_model)
     do.calculate_joint_estimate()
@@ -452,8 +467,9 @@ def main():
     # write deTiN reclassified SSNVs
     do.SSNVs.to_csv(path_or_buf=do.input.output_path + '/' + do.input.output_name + '.deTiN_SSNVs.txt', sep='\t',
                     index=None)
-    if not di.indel_file=='None':
-        do.indels.to_csv(path_or_buf=do.input.output_path + '/' + do.input.output_name + '.deTiN_indels.txt',sep='\t',index=None)
+    if not di.indel_file == 'None':
+        do.indels.to_csv(path_or_buf=do.input.output_path + '/' + do.input.output_name + '.deTiN_indels.txt', sep='\t',
+                         index=None)
     # write plots
     if not np.isnan(ascna_based_model.TiN):
         du.plot_kmeans_info(ascna_based_model, do.input.output_path, do.input.output_name)
