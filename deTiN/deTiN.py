fixing removal of M1 KEEP sites due to high ExAC af and correcting edge case where TiN model and null model both underflow

deTiN/deTiN.py
@@ -333,8 +333,9 @@ class output:
             self.p_null = 1
         pH1 = self.joint_posterior[self.TiN_int]
         pH0 = self.p_null
-        if np.true_divide(self.input.TiN_prior * pH1,
-                          (self.input.TiN_prior * pH1) + ((1 - self.input.TiN_prior) * pH0)) < 0.5:
+        p_model = np.true_divide(self.input.TiN_prior * pH1,
+                          (self.input.TiN_prior * pH1) + ((1 - self.input.TiN_prior) * pH0))
+        if p_model < 0.5 or ~np.isfinite(p_model):
             print 'insufficient evidence to justify TiN > 0'
             self.joint_posterior = np.zeros([self.input.resolution, 1])
             self.joint_posterior[0] = 1

deTiN/deTiN_utilities.py
@@ -677,10 +677,11 @@ def remove_exac_sites_from_call_stats(call_stats_table, exac_file):
         keep = np.ones_like(call_stats_table['position'], dtype=bool)
     for index, row in call_stats_table.iterrows():
         key = str(row['contig']) + '_' + str(row['position'])
-        try:
-            exac_dict[key]
-            # print 'removing site '+ key+ ' minor allele fraction = ' + str(exac_dict[key]['AF'])
-            keep[index] = False
-        except KeyError:
-            pass
+        if row['judgement'] == 'REJECT':
+            try:
+                exac_dict[key]
+                # print 'removing site '+ key+ ' minor allele fraction = ' + str(exac_dict[key]['AF'])
+                keep[index] = False
+            except KeyError:
+                pass
     return call_stats_table[keep]